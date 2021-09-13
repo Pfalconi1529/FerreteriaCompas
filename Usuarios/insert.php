bcd5b206c4f8767bb5210299ce4f176eba69d9ff
Pfalconi1529 <87053346+Pfalconi1529@users.noreply.github.com>
2021-09-12 20:06:02 -0500

    Usuarios con ajax

--- Usuarios/insert.php
+++ Usuarios/insert.php
@@ -1,18 +1,26 @@
-<?php 
-	include_once "../conexion/conexion.php";//incluimos la clase
+<?php
+	include_once('../conexion/conexion.php');
 	$connect = new connect();
 	$connection = $connect->connection();
 
 
-	$nombre = $_POST['nombre'];
-	$cedula = $_POST['cedula'];
-	$email = $_POST['email'];
-	$imagen = $_POST['imagen'];
-
-
-
-	$query = "INSERT INTO usuarios (carId,usuNombre, usuCedula,usuCorreo,usuImagen) VALUES ('$nombre','$cedula','$email','$imagen') ";
-	$result = $connection->prepare($query);
+	$cargo= $_POST['cargo'];
+	$nombre= $_POST['nombre'];
+	$cedula= $_POST['cedula'];
+	$email= $_POST['email'];
+	
+	//empieza tratamiento de imagen
+	$imagen= $_FILES['imagen']['tmp_name'];
+	$imgContenido= addslashes(file_get_contents($imagen));
+	
+	
+	$query= "INSERT INTO ususarios (CARID,USUNOMBRE,USUCEDULA,USUCORREO,USUIMAGEN) values(0,'$cargo','$nombre','$cedula','$email','$imgContenido')";
+	$result= $connection->prepare($query);
 	$result->execute();
+	if ($result>0) {
+		header('Location:Usuarios.php');
+	}
 
+	
  ?>
+ 
